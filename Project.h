3df091d5493ae09bbf79574a2fe427d7f97f3e4c
Eyad <eiad13essam31@gmail.com>
2023-05-12 22:13:20 +0300

    project.h modified

--- Project.h
+++ Project.h
@@ -1,4 +1,16 @@
 #ifndef PROJECT_H_INCLUDED
 #define PROJECT_H_INCLUDED
-#define Max 555
+#define MAX 500
+struct stude{
+    char* name;
+    int id;
+    int age;
+    char gender[10];
+    int grade;
+    char* password;
+}stud[MAX];
+
+void unique_id(int n);
+void add_std(void);
+void view_all_records(void);
 #endif // PROJECT_H_INCLUDED
